afd210dd103beda6531e52ae2faf527da3919bab
MeWRistem <57293583+DeasyXD@users.noreply.github.com>
2022-10-02 12:21:26 +0700

    Update #1 input 2 values in 1 line.py

--- #1 input 2 values in 1 line.py
+++ #1 input 2 values in 1 line.py
@@ -1,7 +1,17 @@
 import math
 x, y = [int(x) for x in input("Enter two values: ").split()]
 z = x/y
-afterdecimal = z%1
-beforedecimal = z//1
+AF = z%1
+string_value = str(AF)
+print(string_value)
+afterdecimal = string_value.replace('.','')
+
+print(type(afterdecimal))
+
+afterdecimal = int(afterdecimal)
+
+print(type(afterdecimal))
+
+beforedecimal = int(z//1)
 #print (afterdecimal, beforedecimal)
 print(afterdecimal+beforedecimal)
