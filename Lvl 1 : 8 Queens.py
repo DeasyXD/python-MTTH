8b5363c3dcc323f59475e066f8fe5bb8c3ff53d6
MeWRistem <57293583+DeasyXD@users.noreply.github.com>
2022-10-08 14:11:53 +0700

    Update Lvl 1 : 8 Queens.py

--- Lvl 1 : 8 Queens.py
+++ Lvl 1 : 8 Queens.py
@@ -1,1 +1,28 @@
+from re import X
+from tkinter.font import BOLD
+import numpy as np
 
+Plot = input("Plot : ") 
+AforTable = "ABCDEFGH"
+NforTable = "12345678"
+
+FirstA = AforTable.find(Plot[0])  + 1
+SecondN = NforTable.find(Plot[1]) + 1
+
+print(FirstA,SecondN)
+
+def printcheckboard(n):
+
+    print("board : ")
+  
+    x = np.zeros((n, n), dtype = object)
+    
+    for i in range(n):
+        for j in range(n):
+            x[i][j] = int(NforTable[i] + NforTable[j])
+            
+    print(x)
+    print(Plot +" : " + str(x[(FirstA-1)][(SecondN-1)]))
+
+n = 8
+printcheckboard(n)
