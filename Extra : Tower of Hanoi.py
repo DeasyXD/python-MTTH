21bb0e83d66a884e84ca949bde2e247748f06b91
MeWRistem <57293583+DeasyXD@users.noreply.github.com>
2022-11-06 19:54:57 +0700

    Update Extra : Tower of Hanoi.py

--- Extra : Tower of Hanoi.py
+++ Extra : Tower of Hanoi.py
@@ -4,7 +4,7 @@ tower3 = []
 
 disks = int(input("Disk : "))
 for i in range(disks, 0, -1):
-    tower2.append(i)
+    tower1.append(i)
 def move(n, source, target, spare):
     if n == 1:
         target.append(source.pop())
@@ -15,5 +15,5 @@ def move(n, source, target, spare):
         move(n-1, spare, target, source)
         
 print(tower1, tower2, tower3)
-move(disks, tower2, tower3, tower1)
+move(disks, tower1, tower3, tower2)
 print("Number of moves: ", (2**disks)-1)
