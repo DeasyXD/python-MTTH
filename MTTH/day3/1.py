1cc3868b27a6cda86f7cb11252a2925731164fd9
MeWRistem <57293583+DeasyXD@users.noreply.github.com>
2022-12-23 15:53:37 +0700

    Add files via upload

--- MTTH/day3/1.py
+++ MTTH/day3/1.py
@@ -1,11 +1,40 @@
-x = 2000
-y = 3200
+t = int(input())
+N = []
+L = []
 
-for i in range(x,y+1):
-    if i%7==0 and i%5!=0:
-        if i == y-1 :
-            print(i,end="")
+
+for i in range(t):
+    n = int(input())
+    l = [int(x) for x in input().split()]
+    N.append(n)
+    L.append(l)
+
+print("n :",N," ","l :",L)
+
+def Ref(a):
+    for i in range(len(a)):
+        # print(a[i])
+        Check(a[i])
+
+def Check(b):
+    default = b[0]
+    for j in range(len(b)):
+        if b[j] < default:
+            default = b[j]
+    for i in range(len(b)):
+        if not CheckForDupe(b):
+            if b[i] != default:
+                b[i] = b[i] - 1
         else:
-            print(i,end = ",")
+            print("index of ",b[i],"is",b.index(b[i]))
+    print(b,default)
 
+
+def CheckForDupe(c):
+    for i in range(0, len(c)):    
+        for j in range(i+1, len(c)): 
+            if c[i] == c[j]:
+                return True
+
+Ref(L)
 
